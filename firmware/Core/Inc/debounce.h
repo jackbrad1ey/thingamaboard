b22ff615d197704ce1426d33d7a0b4fea7700db2
Jack Bradley <jackobradley@icloud.com>
2023-07-23 12:20:30 +1000

    ready for testing

--- firmware/Core/Inc/debounce.h
+++ firmware/Core/Inc/debounce.h
@@ -2,8 +2,8 @@
 #define DEBOUNCE_M
 
 #define DEBOUNCE_RATE 100
-int debounce_array[256] = {0};
-int active_toggle[256] = {0};
+extern int debounce_array[256];
+extern int active_toggle[256];
 
 void debounce_decrement();
 void set_debounce(int keycode);
--- firmware/Core/Src/debounce.c
+++ firmware/Core/Src/debounce.c
@@ -1,9 +1,11 @@
-// stops a keypress from being registered twice within a given interval
 #include "debounce.h"
+
+int debounce_array[256] = {0};
+int active_toggle[256] = {0};
 
 void debounce_decrement() {
     for (int i=0; i < 256; i++) {
-        if (debounce_array[i] == 1 && active_toggle[i] == 1) {
+        if (debounce_array[i] == 1) {
             active_toggle[i] = 0;
         } 
 
